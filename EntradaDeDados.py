3a3f5eedcee01c33cb37d5360dcf54b78a0bdef7
Matheus Campanhola Santos <80859669+MatheusCampanhola@users.noreply.github.com>
2021-03-18 17:08:15 -0300

    Create EntradaDeDados.py

---
+++ EntradaDeDados.py
@@ -0,0 +1,31 @@
+
+'''
+Exemplos de input/print
+
+[1] Exemplo de print antigo (2.x)
+
+print('Qual seu nome? ')
+nome = input('R: ')
+
+print('Seja bem vindo(a) %s' % nome)
+
+print('Qual sua idade? ')
+idade = input('R: ')
+
+print('A %s tem %s anos.' % (nome, idade))
+
+[2] Exemplo de print moderno (3.x)
+
+print(f'Qual seu nome?')
+nome = input('R: ')
+
+print(f'Seja bem vindo(a) {nome}') ou
+print(''Seja bem vindo(a) {}' .format(nome)) f = .format
+
+print('Qual sua idade? ')
+idade = input('R: ')
+
+print(f'A {nome} tem {idade} anos.') ou
+print(''A {} tem {} anos.' .format(nome, idade)) f = .format
+
+'''

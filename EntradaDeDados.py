74b2a7ade092e2d3005c8ce9a03e60c06d763aa6
Matheus Campanhola Santos <80859669+MatheusCampanhola@users.noreply.github.com>
2021-03-18 17:19:17 -0300

    Update EntradaDeDados.py

--- EntradaDeDados.py
+++ EntradaDeDados.py
@@ -28,4 +28,16 @@ idade = input('R: ')
 print(f'A {nome} tem {idade} anos.') ou
 print(''A {} tem {} anos.' .format(nome, idade)) f = .format
 
+[3] Exemplo de print "mais moderno"
+
+print(f'A {nome} tem {2018 - int(idade)') -> cast
+
+Cast -> conversão de um tipo de dado para outro.
+
+[4] Forma mais correta de se usar
+
+nome = int(input("Qual seu nome?\n")) -> \n pula linha
+idade = int(input("Qual sua idade?\n")) -> \n pula linha
+
+print(f'Bem vinda(a) {nome}, você tem {idade} anos.')
 '''

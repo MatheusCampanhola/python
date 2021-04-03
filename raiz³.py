8b6deafeceb166b77a108d12cf9cf41d61de4503
Matheus Campanhola Santos <80859669+MatheusCampanhola@users.noreply.github.com>
2021-04-03 17:39:47 -0300

    Update raiz³.py

--- raiz³.py
+++ raiz³.py
@@ -1,4 +1,4 @@
-numero = float(input('Número: ')) // pode ser int ou float, math só calcula até raiz²  
+numero = float(input('Número: ')) # pode ser int ou float, math só calcula até raiz²  
 raiz2 = numero ** (1/2)  
 raiz3 = numero ** (1/3)  
 print(f'A raiz quadrada foi {raiz2:.2f} e a raiz cúbica foi {raiz3:.2f}')  
